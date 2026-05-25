8f83e8bb6341d07163cca4cdcc9e3a44bbb64be0
Ganesh Bucche <ganesh.n.bucche@gmail.com>
2026-05-25 21:05:03 +0530

    First commit

---
+++ factrecursion.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class factrecursion {
+
+    public static int fact(int n) {
+
+        if(n == 0 || n == 1) {
+            return 1;
+        }
+
+        return n * fact(n - 1);
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        int result = fact(n);
+
+        System.out.println("Factorial = " + result);
+    }
+}
