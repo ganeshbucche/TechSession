1749464a4a8d69cb490cd656615e21619ad5f851
Ganesh Bucche <ganesh.n.bucche@gmail.com>
2026-05-25 20:20:55 +0530

    First commit

---
+++ reverse.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class reverse{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in); 
+
+        System.out.print("Enter a 3 digit number: ");
+        int num = sc.nextInt();
+
+        int first = num / 100;
+        int second = (num / 10) % 10;
+        int third = num % 10;
+
+        int reverse = (third* 100)+(second * 10) + first;
+
+        System.out.println("Reversed number is : "+ reverse);
+    }
+}
